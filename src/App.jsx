62ff9274bad414627c6e33492b4ff387c78ad3b5
TasyDev <tamayocontat@gmail.com>
2025-12-24 18:48:48 -0500

    Refactor: Create organational struture proyect

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import TodoCounter from "./components/TodoCounter.jsx";
+import TodoSearch from "./components/TodoSearch.jsx";
+import ToDoList from "./components/ToDoList.jsx";
+import TodoItem from "./components/TodoItem.jsx";
+import CreateToDo from "./components/CreateToDo.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-          <TodoItem />
+    <main>
+        <TodoCounter />
+        <TodoSearch />
 
-          <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+        <ToDoList>
+            <TodoItem />
+        </ToDoList>
+
+        <CreateToDo />
+    </main>
   )
-}
-
-function TodoItem() {
-    return(
-      <li>
-          <span>Complete</span>
-          <p>La tarea</p>
-          <span>X</span>
-      </li>
-    );
 }
 
 export default App;
